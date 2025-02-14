6c78c701b3c9bb27a3c7bc79e66b520cdff9baf3
Ganesh <ganeshpodishetti@gmail.com>
2025-02-13 22:35:19 -0500

    flexible data connection

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -6,4 +6,6 @@ public class Dish
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public decimal Price { get; set; }
+    
+    public int RestaurantId { get; set; }
 }
---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Infrastructure.Context;
+
+namespace Restaurants.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("RestaurantDbConnection");
+        services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(connectionString));
+    }
+}
